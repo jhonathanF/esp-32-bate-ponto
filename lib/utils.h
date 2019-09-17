0d955b82b074a260a814ab92a730913414fbd431
Raian <raian_hgp@hotmail.com>
2019-09-17 12:00:38 -0300

    MyEEPROM e Utils

---
+++ lib/utils.h
@@ -0,0 +1,21 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+
+#include <RTClib.h>
+#include <stdint.h>
+#include "../lib/MyEEPROM.h"
+
+
+RTC_DS1307 rtc;
+
+
+
+void attBufferRegister();
+
+
+
+
+
+
+#endif
