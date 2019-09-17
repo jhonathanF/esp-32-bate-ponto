3021a6e71a6f0a2c92d98759c80c539eb64fd229
Raian <raian_hgp@hotmail.com>
2019-09-17 12:30:09 -0300

    up

--- lib/utils.h
+++ lib/utils.h
@@ -7,6 +7,17 @@
 
 RTC_DS1307 rtc;
 
-void attBufferRegister();
+typedef struct time{
+    uint8_t ano;
+    uint8_t mes;
+    uint8_t dia;
+    uint8_t hora;
+    uint8_t minuto;
+    uint8_t segundo;
+} time;
+
+time CurrentTime;
+
+void adjustDateTime(int ano, int mes, int dia, int hora, int minuto, int segundo);
 
 #endif
